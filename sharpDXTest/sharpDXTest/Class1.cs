21eaed6df474156a6352d6d51d30f81160e360f4
beta <chris@wirmachenbunt.de>
2017-11-23 17:40:57 +0100

    init

---
+++ sharpDXTest/sharpDXTest/Class1.cs
@@ -0,0 +1,86 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Windows.Forms;
+using System.Collections.Generic;
+//using System.Drawing;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using SharpDX.Direct2D1;
+using SharpDX.DXGI;
+using SharpDX;
+
+using VVVV.Core.Logging;
+#endregion usings
+
+namespace VVVV.Nodes
+{
+    #region PluginInfo
+    [PluginInfo(Name = "sharpDXTest",
+                Category = "GUI",
+                Help = "Template with some gui elements",
+                Tags = "c#",
+                AutoEvaluate = true)]
+    #endregion PluginInfo
+
+    public class GUITemplateNode : UserControl, IPluginEvaluate
+    {
+        #region fields & pins
+
+        [Import()]
+        public ILogger FLogger;
+
+        public SharpDX.Direct2D1.Factory Factory2D { get; private set; }
+        public SharpDX.DirectWrite.Factory FactoryDWrite { get; private set; }
+        public WindowRenderTarget RenderTarget2D { get; private set; }
+        public SolidColorBrush SceneColorBrush { get; private set; }
+
+
+        #endregion fields & pins
+
+        #region constructor and init
+
+        public GUITemplateNode()
+        {
+            //setup the gui
+            InitializeComponent();
+        }
+
+        void InitializeComponent()
+        {
+            Factory2D = new SharpDX.Direct2D1.Factory();
+            FactoryDWrite = new SharpDX.DirectWrite.Factory();
+            HwndRenderTargetProperties properties = new HwndRenderTargetProperties();
+            properties.Hwnd = this.Handle;
+            properties.PixelSize = new SharpDX.Size2(1, 1);
+            properties.PresentOptions = PresentOptions.None;
+
+            RenderTarget2D = new WindowRenderTarget(Factory2D, new RenderTargetProperties(new PixelFormat(Format.Unknown, SharpDX.Direct2D1.AlphaMode.Premultiplied)), properties);
+
+            RenderTarget2D.AntialiasMode = AntialiasMode.PerPrimitive;
+
+
+            //SceneColorBrush = new SolidColorBrush(RenderTarget2D, System.Drawing.Color.White);
+            //NOT WORKING
+        }
+
+
+
+        #endregion constructor and init
+
+        //called when data for any output pin is requested
+        public void Evaluate(int SpreadMax)
+        {
+            RenderTarget2D.BeginDraw();
+
+            //not drawing anything yet
+
+            RenderTarget2D.EndDraw();
+
+        }
+    }
+}
---
+++ sharpDXTest/sharpDXTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("sharpDXTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sharpDXTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("ad910140-f02e-4646-8395-6025947a83a0")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
